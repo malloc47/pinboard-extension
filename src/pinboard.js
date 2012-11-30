375be70d23b8fe4bf2b4f5fdf45aea4ee0648c33
Jarrell Waggoner <malloc47@gmail.com>
2012-11-30 01:32:13 -0500

    quick conversion of the pinboard bookmarklet to a chrome extension

---
+++ src/pinboard.js
@@ -0,0 +1,1 @@
+javascript:(function(){ _my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='http://localhost:8080/js/bookmarklet.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(_my_script);})();
