a020894c3216a958eebbcf71a97d503fada67d62
Jarrell Waggoner <malloc47@gmail.com>
2012-11-30 01:50:29 -0500

    cleanup

--- src/background.js
+++ src/background.js
@@ -1,10 +1,3 @@
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse(settings.toObject());
-  });
-
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {file: "src/pinboard.js"})
 });
